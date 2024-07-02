64221b718ce7b2045ca0e1ca86e63a82b8937720
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-02 21:03:53 +0630

    [Update]

--- MTKDotNetCore.ConsoleApp/AdoDotNetExample.cs
+++ MTKDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -86,7 +86,34 @@ namespace MTKDotNetCore.ConsoleApp
 
             int result = cmd.ExecuteNonQuery();
             connection.Close();
-            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            string message = result > 0 ? "Saving Successful" : "Saving Fail";
+
+            Console.WriteLine(message);
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update(int id,string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection( _sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"Update[dbo].[Tbl_Blog] 
+            SET[BlogTitle] = @BlogTitle
+            ,[BlogAuthor] = @BlogAuthor
+            ,[BlogContent] = @BlogContent Where BlogId = @BlogId";
+
+            SqlCommand cmd = new SqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+
+
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Updating Successful" : "Updating Fail";
 
             Console.WriteLine(message);
         }
