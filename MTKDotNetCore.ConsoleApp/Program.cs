c4355ee13fa48692f4c371803408562142c674a7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-02 10:45:35 +0630

    [Adding ConnectionString]

--- MTKDotNetCore.ConsoleApp/Program.cs
+++ MTKDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+
+SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=OJTBatch1;Integrated Security=True;TrustServerCertificate=True");
+
+connection.Open();
+Console.WriteLine("Connection Open");
+connection.Close();
+Console.WriteLine("Connection Close");
+
+Console.ReadKey();
