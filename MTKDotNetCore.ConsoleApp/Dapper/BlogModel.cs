e34ed1fedb309333bd10358690f117554df01804
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-03 22:06:38 +0630

    -

--- MTKDotNetCore.ConsoleApp/Dapper/BlogModel.cs
+++ MTKDotNetCore.ConsoleApp/Dapper/BlogModel.cs
@@ -10,11 +10,15 @@ using System.Threading.Tasks;
 
 namespace MTKDotNetCore.ConsoleApp.Dapper;
 
+#region BlogModel
 public class BlogModel
 {
-    public long BlogId {  get; set; }
-    public string BlogTitle {  get; set; }
-    public string BlogAuthor {  get; set; }
-    public string BlogContent {  get; set; }
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
 //public record BlogEntity(int BlogId, string BlogTitle,string BlogAuthor,string BlogContent);
+
+
+#endregion
