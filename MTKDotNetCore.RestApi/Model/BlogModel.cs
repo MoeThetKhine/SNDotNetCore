344b129a69e092b9c79e042e83f1e351258d84dc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-04 17:42:00 +0630

    -

--- MTKDotNetCore.RestApi/Model/BlogModel.cs
+++ MTKDotNetCore.RestApi/Model/BlogModel.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿#region using
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
+#endregion
 
 namespace MTKDotNetCore.RestApi.Model
 {
