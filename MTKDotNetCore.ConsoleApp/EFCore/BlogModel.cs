a274ee3a8e00466d7e12c4082ff1ca83d79625c2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-04 12:13:05 +0630

    -

--- MTKDotNetCore.ConsoleApp/EFCore/BlogModel.cs
+++ MTKDotNetCore.ConsoleApp/EFCore/BlogModel.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿#region using 
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#endregion
+
+
+
 
 namespace MTKDotNetCore.ConsoleApp.EFCore
 {
