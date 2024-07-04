924bfb6dd48bb3d99b03fbbeae26a3d9245c28eb
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-04 12:14:36 +0630

    -

--- MTKDotNetCore.ConsoleApp/EFCore/AppDbContext.cs
+++ MTKDotNetCore.ConsoleApp/EFCore/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace MTKDotNetCore.ConsoleApp.EFCore
 {
     public class AppDbContext: DbContext
     {
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
