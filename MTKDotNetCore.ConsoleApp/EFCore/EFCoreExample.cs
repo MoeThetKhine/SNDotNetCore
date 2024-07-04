5f4d680ecef329710ea6666f6229faba1080d0bc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-04 11:53:49 +0630

    -

--- MTKDotNetCore.ConsoleApp/EFCore/EFCoreExample.cs
+++ MTKDotNetCore.ConsoleApp/EFCore/EFCoreExample.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿#region using 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#endregion
 
 namespace MTKDotNetCore.ConsoleApp.EFCore
 {
