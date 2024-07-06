5ae7bdf68edf0ddbc6d193120c4a732253df469c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-06 11:52:48 +0630

    CreateBlog

--- MTKDotNetCore.AdoDotNetWebApi/Controllers/BlogAdoDotNetController.cs
+++ MTKDotNetCore.AdoDotNetWebApi/Controllers/BlogAdoDotNetController.cs
@@ -5,80 +5,109 @@ using System.Data;
 using System.Data.SqlClient;
 using MTKDotNetCore.AdoDotNetWebApi.Database;
 
-namespace MTKDotNetCore.AdoDotNetWebApi.Controllers
+namespace MTKDotNetCore.AdoDotNetWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogAdoDotNetController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogAdoDotNetController : ControllerBase
+
+    [HttpGet]
+    public IActionResult GetBlogs()
     {
+        string query = "select * from Tbl_Blog";
+        SqlConnection connection = new SqlConnection(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sqlDataAdapter.Fill(dt);
+        connection.Close();
 
-        [HttpGet]
-        public IActionResult GetBlogs()
+        //List<BlogModel> lst = new List<BlogModel>();
+        //foreach(DataRow dr  in dt.Rows)
+        //{
+        //    BlogModel blog = new BlogModel();
+        //    blog.BlogId = Convert.ToInt32(dr["BlogId"]);
+        //    blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
+        //    blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
+        //    blog.BlogContent = Convert.ToString(dr["BlogContent"]);
+        //    blog.IsActive = Convert.ToBoolean(dr["IsActive"]);
+        //    lst.Add(blog);
+        //}
+
+        List<BlogModel> lst = dt.AsEnumerable().Select(dr => new BlogModel
         {
-            string query = "select * from Tbl_Blog";
-            SqlConnection connection = new SqlConnection(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(query, connection);
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sqlDataAdapter.Fill(dt);
-            connection.Close();
+            BlogId = Convert.ToInt32(dr["BlogId"]),
+            BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            BlogContent = Convert.ToString(dr["BlogContent"]),
+            IsActive = Convert.ToBoolean(dr["IsActive"])
 
-            //List<BlogModel> lst = new List<BlogModel>();
-            //foreach(DataRow dr  in dt.Rows)
-            //{
-            //    BlogModel blog = new BlogModel();
-            //    blog.BlogId = Convert.ToInt32(dr["BlogId"]);
-            //    blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
-            //    blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
-            //    blog.BlogContent = Convert.ToString(dr["BlogContent"]);
-            //    blog.IsActive = Convert.ToBoolean(dr["IsActive"]);
-            //    lst.Add(blog);
-            //}
+        }).ToList();
+        return Ok(lst);
+    }
 
-            List<BlogModel>lst = dt.AsEnumerable().Select(dr => new BlogModel
-            {
-                BlogId = Convert.ToInt32(dr["BlogId"]),
-                BlogTitle = Convert.ToString(dr["BlogTitle"]),
-                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
-                BlogContent = Convert.ToString(dr["BlogContent"]),
-                IsActive = Convert.ToBoolean(dr["IsActive"])
 
-            }).ToList();
-            return Ok(lst);
+    [HttpGet("{id}")]
+    public IActionResult EditBlog(int id)
+    {
+        string query = "select * from Tbl_Blog WHERE BlogId = @BlogId";
+        SqlConnection connection = new SqlConnection(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogId", id);
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sqlDataAdapter.Fill(dt);
+        connection.Close();
+
+        if (dt.Rows.Count == 0)
+        {
+            return NotFound("No Data Found");
         }
 
+        DataRow dr = dt.Rows[0];
 
-        [HttpGet("{id}")]
-        public IActionResult EditBlog(int id)
+        var item = new BlogModel
         {
-            string query = "select * from Tbl_Blog WHERE BlogId = @BlogId";
-            SqlConnection connection = new SqlConnection(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(query, connection);
-            cmd.Parameters.AddWithValue("@BlogId", id);
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sqlDataAdapter.Fill(dt);
-            connection.Close();
+            BlogId = Convert.ToInt32(dr["BlogId"]),
+            BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            BlogContent = Convert.ToString(dr["BlogContent"]),
+            IsActive = Convert.ToBoolean(dr["IsActive"])
 
-            if (dt.Rows.Count == 0)
-                    {
-                        return NotFound("No Data Found");
-                    }
-                    DataRow dr = dt.Rows[0];
+        };
+        return Ok(item);
+    }
+    [HttpPost]
+    public IActionResult CreateBlog(BlogModel blog)
+    {
+        string query = @"INSERT INTO [dbo].[Tbl_Blog]
+        ([BlogTitle]
+              ,[BlogAuthor]
+              ,[BlogContent])
 
-            var item = new BlogModel
-            {
-                BlogId = Convert.ToInt32(dr["BlogId"]),
-                BlogTitle = Convert.ToString(dr["BlogTitle"]),
-                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
-                BlogContent = Convert.ToString(dr["BlogContent"]),
-                IsActive = Convert.ToBoolean(dr["IsActive"])
-
-            };
-            return Ok(item);
-        }
+                VALUES
+              (@BlogTitle
+               ,@BlogAuthor
+               ,@BlogContent)";
+        SqlConnection connection = new SqlConnection(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+        cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+        cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+        int result = cmd.ExecuteNonQuery();
+        connection.Close();
+        string message = result > 0 ? "Saving Successful" : "Saving Fail";
+        return Ok(message);
 
     }
+
 }
+
+
+
+
+
