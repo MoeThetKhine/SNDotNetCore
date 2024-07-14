79a4b98592efc29665be2c6490518298accecedf
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-14 21:08:12 +0630

    Format Code

--- MTKDotNetCore.DapperCustomService/Database/AppDbContext.cs
+++ MTKDotNetCore.DapperCustomService/Database/AppDbContext.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MTKDotNetCore.DapperCustomService.Model;
 
-namespace MTKDotNetCore.DapperCustomService.Database
+namespace MTKDotNetCore.DapperCustomService.Database;
+
+internal class AppDbContext : DbContext
 {
-    internal class AppDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-        }
-        public DbSet<BlogModel> Blog { get; set; }
+        optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
     }
 
+    public DbSet<BlogModel> Blog { get; set; }
 }
