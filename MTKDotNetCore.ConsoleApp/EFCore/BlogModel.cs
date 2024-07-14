dfc46ad17c6ef3c1a1609b2f7f5b87a8b4b313f4
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-14 21:00:47 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp/EFCore/BlogModel.cs
+++ MTKDotNetCore.ConsoleApp/EFCore/BlogModel.cs
@@ -1,25 +1,19 @@
-﻿#region using 
+﻿#region using
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 #endregion
 
-namespace MTKDotNetCore.ConsoleApp.EFCore
+namespace MTKDotNetCore.ConsoleApp.EFCore;
+
+[Table("Tbl_Blog")]
+public class BlogModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId {  get; set; }
-        public string BlogTitle {  get; set; }
-        public string BlogAuthor {  get; set; } 
-        public string BlogContent {  get; set; }
-        public bool IsActive {  get; set; }
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
 }
