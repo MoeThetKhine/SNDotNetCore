4828a25937e27d356f764e79d85b5d4c16af3d54
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-14 21:01:24 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp/EFCore/EFCoreExample.cs
+++ MTKDotNetCore.ConsoleApp/EFCore/EFCoreExample.cs
@@ -1,125 +1,120 @@
-﻿#region using 
+﻿namespace MTKDotNetCore.ConsoleApp.EFCore;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-#endregion
-
-namespace MTKDotNetCore.ConsoleApp.EFCore
+public class EFCoreExample
 {
-    public class EFCoreExample
+    private readonly AppDbContext db = new AppDbContext();
+
+    #region Run
+
+    public void Run()
     {
-        private readonly AppDbContext db = new AppDbContext();
+        Read();
+        // Edit(2);
+        // Edit(22);
+        // Create("Title created", "Author created", "Content created");
+        // Update(14,"Title updated", "Author updated", "Content updated");
+        Delete(14);
+    }
 
-        #region Run
-        public void Run()
-        {
-            Read();
-            // Edit(2);
-            // Edit(22);
-            // Create("Title created", "Author created", "Content created");
-            // Update(14,"Title updated", "Author updated", "Content updated");
-            Delete(14);
-        }
-        #endregion
+    #endregion
 
-        #region Read
-        private void Read()
-        {
-            var lst = db.Blogs.ToList();
-            foreach (BlogModel item in lst)
-            {
-                Console.WriteLine(item.BlogId);
-                Console.WriteLine(item.BlogTitle);
-                Console.WriteLine(item.BlogAuthor);
-                Console.WriteLine(item.BlogContent);
-                Console.WriteLine("_________________________");
-            }
-        }
-        #endregion
+    #region Read
 
-        #region Edit
-        private void Edit(int id)
+    private void Read()
+    {
+        var lst = db.Blogs.ToList();
+        foreach (BlogModel item in lst)
         {
-            var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
-            //foreach(BlogModel x in db.Blogs)
-            //{
-            //    if (x.BlogId == id)
-            // }
-            if (item is null)
-            {
-                Console.WriteLine("No Data Found");
-                return;
-            }
             Console.WriteLine(item.BlogId);
             Console.WriteLine(item.BlogTitle);
             Console.WriteLine(item.BlogAuthor);
             Console.WriteLine(item.BlogContent);
-            Console.WriteLine("____________________");
+            Console.WriteLine("_________________________");
         }
-
-        #endregion
-
-        #region Create
-
-        private void Create(string title, string author, string content)
-        {
-            var item = new BlogModel
-            {
-                BlogTitle = title,
-                BlogAuthor = author,
-                BlogContent = content
-            };
-            db.Blogs.Add(item);
-            int result = db.SaveChanges();
-
-            string message = result > 0 ? "Creating Successful" : "Creating Fail";
-            Console.WriteLine(message);
-        }
-        #endregion
-
-        #region Update
-        private void Update(int id, string title, string author, string content)
-        {
-            var item = db.Blogs.FirstOrDefault(x => x.BlogId == id); // id exists or not
-
-            if (item is null)
-            {
-                Console.WriteLine("No Data Found");
-                return;
-            }
-            item.BlogTitle = title;
-            item.BlogAuthor = author;
-            item.BlogContent = content;
-
-            int result = db.SaveChanges();
-
-            string message = result > 0 ? "Updating Successful" : "Updating Fail";
-            Console.WriteLine(message);
-        }
-
-
-        #endregion
-
-        #region Delete
-        private void Delete(int id)
-        {
-
-            var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
-            if (item is null)
-            {
-                Console.WriteLine("No Data Found");
-            }
-            db.Blogs.Remove(item);
-            int result = db.SaveChanges();
-
-            string message = result > 0 ? "Deleting Successful" : "Deleting Fail";
-            Console.WriteLine(message);
-        }
-
-        #endregion
     }
+
+    #endregion
+
+    #region Edit
+
+    private void Edit(int id)
+    {
+        var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
+        //foreach(BlogModel x in db.Blogs)
+        //{
+        //    if (x.BlogId == id)
+        // }
+        if (item is null)
+        {
+            Console.WriteLine("No Data Found");
+            return;
+        }
+        Console.WriteLine(item.BlogId);
+        Console.WriteLine(item.BlogTitle);
+        Console.WriteLine(item.BlogAuthor);
+        Console.WriteLine(item.BlogContent);
+        Console.WriteLine("____________________");
+    }
+
+    #endregion
+
+    #region Create
+
+    private void Create(string title, string author, string content)
+    {
+        var item = new BlogModel
+        {
+            BlogTitle = title,
+            BlogAuthor = author,
+            BlogContent = content
+        };
+        db.Blogs.Add(item);
+        int result = db.SaveChanges();
+
+        string message = result > 0 ? "Creating Successful" : "Creating Fail";
+        Console.WriteLine(message);
+    }
+    #endregion
+
+    #region Update
+
+    private void Update(int id, string title, string author, string content)
+    {
+        var item = db.Blogs.FirstOrDefault(x => x.BlogId == id); // id exists or not
+
+        if (item is null)
+        {
+            Console.WriteLine("No Data Found");
+            return;
+        }
+        item.BlogTitle = title;
+        item.BlogAuthor = author;
+        item.BlogContent = content;
+
+        int result = db.SaveChanges();
+
+        string message = result > 0 ? "Updating Successful" : "Updating Fail";
+        Console.WriteLine(message);
+    }
+
+    #endregion
+
+    #region Delete
+
+    private void Delete(int id)
+    {
+
+        var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            Console.WriteLine("No Data Found");
+        }
+        db.Blogs.Remove(item);
+        int result = db.SaveChanges();
+
+        string message = result > 0 ? "Deleting Successful" : "Deleting Fail";
+        Console.WriteLine(message);
+    }
+
+    #endregion
 }
