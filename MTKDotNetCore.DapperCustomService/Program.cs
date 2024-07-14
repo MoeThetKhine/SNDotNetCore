20c28883f03751941dd07d1298f0065bc17d9ccc
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-14 21:09:13 +0630

    Format Code

--- MTKDotNetCore.DapperCustomService/Program.cs
+++ MTKDotNetCore.DapperCustomService/Program.cs
@@ -1,11 +1,7 @@
 using MTKDotNetCore.DapperCustomService.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +9,6 @@ builder.Services.AddScoped(n => new DapperService(builder.Configuration.GetConne
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
