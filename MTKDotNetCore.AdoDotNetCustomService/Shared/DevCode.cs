c440dda29c17fa9a94b2cfddecc4651885534852
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-14 21:05:52 +0630

    Format Code

--- MTKDotNetCore.AdoDotNetCustomService/Shared/DevCode.cs
+++ MTKDotNetCore.AdoDotNetCustomService/Shared/DevCode.cs
@@ -1,24 +1,23 @@
-﻿using Newtonsoft.Json;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
+using Newtonsoft.Json;
 using static MTKDotNetCore.AdoDotNetCustomService.Shared.AdoDotNetService;
 
-namespace MTKDotNetCore.AdoDotNetCustomService.Shared
+namespace MTKDotNetCore.AdoDotNetCustomService.Shared;
+
+public static class DevCode
 {
-    public static class DevCode
+    public static string SerializeObject(this object obj)
     {
-        public static string SerializeObject(this object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+        return JsonConvert.SerializeObject(obj);
+    }
 
-        public static T DeserializeObject<T>(this string jsonStr)
-        {
-            return JsonConvert.DeserializeObject<T>(jsonStr)!;
-        }
+    public static T DeserializeObject<T>(this string jsonStr)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonStr)!;
+    }
 
-        public static SqlParameter Map(this AdoDotNetParameter parameter)
-        {
-            return new SqlParameter(parameter.Name, parameter.Value);
-        }
+    public static SqlParameter Map(this AdoDotNetParameter parameter)
+    {
+        return new SqlParameter(parameter.Name, parameter.Value);
     }
 }
