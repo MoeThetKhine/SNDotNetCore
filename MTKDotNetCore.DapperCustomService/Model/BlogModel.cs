da15263d6da13bad46c5a7d83beab72088d13619
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:47:42 +0630

    Feat : Add Global Using

--- MTKDotNetCore.DapperCustomService/Model/BlogModel.cs
+++ MTKDotNetCore.DapperCustomService/Model/BlogModel.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace MTKDotNetCore.DapperCustomService.Model;
+﻿namespace MTKDotNetCore.DapperCustomService.Model;
 
 [Table("Tbl_Blog")]
 public class BlogModel
