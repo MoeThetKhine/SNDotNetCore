cbd445112da8af88e7eb4717effd6627156424c9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:20:22 +0630

    -

--- MTKDotNetCore.AdoDotNetWebApi/Model/BlogModel.cs
+++ MTKDotNetCore.AdoDotNetWebApi/Model/BlogModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace MTKDotNetCore.AdoDotNetWebApi.Model;
 
+#region BlogModel
+
 public class BlogModel
 {
     public long BlogId { get; set; }
@@ -8,3 +10,5 @@ public class BlogModel
     public string BlogContent { get; set; }
     public bool IsActive { get; set; }
 }
+
+#endregion
