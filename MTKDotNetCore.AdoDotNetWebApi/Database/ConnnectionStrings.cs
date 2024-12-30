04c154ff2ce4845e5a1492867079e6a7f12ffdc9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:15:00 +0630

    -

--- MTKDotNetCore.AdoDotNetWebApi/Database/ConnnectionStrings.cs
+++ MTKDotNetCore.AdoDotNetWebApi/Database/ConnnectionStrings.cs
@@ -1,14 +1,17 @@
 ﻿namespace MTKDotNetCore.AdoDotNetWebApi.Database;
 
+#region ConnnectionStrings
+
 public class ConnnectionStrings
 {
     
-        public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-        {
-            DataSource = "localhost", // or "(local)" or your server name
-            InitialCatalog = "OJTBatch1",
-            IntegratedSecurity = true,
-            TrustServerCertificate = true,
-        };
-    
+    public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = "localhost", // or "(local)" or your server name
+        InitialCatalog = "OJTBatch1",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true,
+    };
 }
+
+#endregion
