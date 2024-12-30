23ca29a7da80a00e2070b63775ea48353e62db98
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:16:09 +0630

    Format Code

--- MTKDotNetCore.AdoDotNetWebApi/Database/AppDbContext.cs
+++ MTKDotNetCore.AdoDotNetWebApi/Database/AppDbContext.cs
@@ -1,12 +1,10 @@
-﻿namespace MTKDotNetCore.AdoDotNetWebApi.Database
+﻿namespace MTKDotNetCore.AdoDotNetWebApi.Database;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-           optionsBuilder.UseSqlServer(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-        }
-        public DbSet<BlogModel> Blogs { get; set; }
+       optionsBuilder.UseSqlServer(ConnnectionStrings._sqlConnectionStringBuilder.ConnectionString);
     }
+    public DbSet<BlogModel> Blogs { get; set; }
 }
- 
