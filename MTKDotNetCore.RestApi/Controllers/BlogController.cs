410b6e83c4315f038dd94f783d585f645fb40a94
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:30:54 +0630

    Format Code

--- MTKDotNetCore.RestApi/Controllers/BlogController.cs
+++ MTKDotNetCore.RestApi/Controllers/BlogController.cs
@@ -1,110 +1,109 @@
-﻿namespace MTKDotNetCore.EFCoreRestApi.Controllers
+﻿namespace MTKDotNetCore.EFCoreRestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public BlogController()
     {
-        private readonly AppDbContext _context;
+        _context = new AppDbContext();
+    }
 
-        public BlogController()
+    #region Read
+    [HttpGet]
+    public IActionResult Read()
+    {
+        var lst = _context.Blogs.ToList();
+        return Ok(lst);
+    }
+    #endregion
+
+    #region Edit
+
+    [HttpGet("{id}")]
+    public IActionResult Edit(int id)
+    {
+        var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
         {
-            _context = new AppDbContext();
+            return NotFound("No Data Found");
         }
+        return Ok(item);
+    }
+    #endregion
 
-        #region Read
-        [HttpGet]
-        public IActionResult Read()
+    #region Create
+
+    [HttpPost]
+    public IActionResult Create(BlogModel blog)
+    {
+        _context.Blogs.Add(blog);
+        var result = _context.SaveChanges();
+        string message = result > 0 ? "Creating Successful" : "Creating Fail";
+        return Ok(message);
+    }
+
+    #endregion
+
+    #region Update
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, BlogModel blog)
+    {
+        var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
         {
-            var lst = _context.Blogs.ToList();
-            return Ok(lst);
+            return NotFound("No Data Found");
         }
-        #endregion
+        item.BlogTitle = blog.BlogTitle;
+        item.BlogAuthor = blog.BlogAuthor;
+        item.BlogContent = blog.BlogContent;
 
-        #region Edit
+        var result = _context.SaveChanges();
+        string message = result > 0 ? "Updating Successful" : "Updating Fail";
+        return Ok(message);
+    }
+    #endregion
 
-        [HttpGet("{id}")]
-        public IActionResult Edit(int id)
+    #region Patch
+
+    [HttpPatch("{id}")]
+    public IActionResult Patch(int id, BlogModel blog)
+    {
+        var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
         {
-            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
-            if (item is null)
-            {
-                return NotFound("No Data Found");
-            }
-            return Ok(item);
+            return NotFound("No Data Found");
         }
-        #endregion
-
-        #region Create
-
-        [HttpPost]
-        public IActionResult Create(BlogModel blog)
-        {
-            _context.Blogs.Add(blog);
-            var result = _context.SaveChanges();
-            string message = result > 0 ? "Creating Successful" : "Creating Fail";
-            return Ok(message);
-        }
-
-        #endregion
-
-        #region Update
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, BlogModel blog)
-        {
-            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
-            if (item is null)
-            {
-                return NotFound("No Data Found");
-            }
+        if (!string.IsNullOrEmpty(blog.BlogTitle))
             item.BlogTitle = blog.BlogTitle;
+        if (!string.IsNullOrEmpty(blog.BlogAuthor))
             item.BlogAuthor = blog.BlogAuthor;
+        if (!string.IsNullOrEmpty(blog.BlogContent))
             item.BlogContent = blog.BlogContent;
 
-            var result = _context.SaveChanges();
-            string message = result > 0 ? "Updating Successful" : "Updating Fail";
-            return Ok(message);
-        }
-        #endregion
-
-        #region Patch
-
-        [HttpPatch("{id}")]
-        public IActionResult Patch(int id, BlogModel blog)
-        {
-            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
-            if (item is null)
-            {
-                return NotFound("No Data Found");
-            }
-            if (!string.IsNullOrEmpty(blog.BlogTitle))
-                item.BlogTitle = blog.BlogTitle;
-            if (!string.IsNullOrEmpty(blog.BlogAuthor))
-                item.BlogAuthor = blog.BlogAuthor;
-            if (!string.IsNullOrEmpty(blog.BlogContent))
-                item.BlogContent = blog.BlogContent;
-
-            var result = _context.SaveChanges();
-            string message = result > 0 ? "Updating Successful" : "Updating Fail";
-            return Ok(message);
-        }
-        #endregion
-
-        #region Delete
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
-            if (item is null)
-            {
-                return NotFound("No Data Found");
-            }
-            _context.Blogs.Remove(item);
-            var result = _context.SaveChanges();
-            string message = result > 0 ? "Deleting Successful" : "Deleting Fail";
-            return Ok(message);
-        }
-        #endregion
+        var result = _context.SaveChanges();
+        string message = result > 0 ? "Updating Successful" : "Updating Fail";
+        return Ok(message);
     }
+    #endregion
+
+    #region Delete
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            return NotFound("No Data Found");
+        }
+        _context.Blogs.Remove(item);
+        var result = _context.SaveChanges();
+        string message = result > 0 ? "Deleting Successful" : "Deleting Fail";
+        return Ok(message);
+    }
+    #endregion
 }
