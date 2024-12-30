3d557568174cf16757720c8d8f7ec0807a8d4d60
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:22:34 +0630

    Format Code

--- MTKDotNetCore.DapperRestApi/Model/BlogModel.cs
+++ MTKDotNetCore.DapperRestApi/Model/BlogModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCore.DapperRestApi.Model
+﻿namespace MTKDotNetCore.DapperRestApi.Model;
+
+[Table("Tbl_Blog")]
+public class BlogModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
 }
