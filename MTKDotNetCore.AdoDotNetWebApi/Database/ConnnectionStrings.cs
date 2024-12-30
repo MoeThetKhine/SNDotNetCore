fb0bddf45e9d1ecb753689d8d95f63abeb8fe577
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:14:27 +0630

    Format Code

--- MTKDotNetCore.AdoDotNetWebApi/Database/ConnnectionStrings.cs
+++ MTKDotNetCore.AdoDotNetWebApi/Database/ConnnectionStrings.cs
@@ -1,16 +1,14 @@
-﻿
-namespace MTKDotNetCore.AdoDotNetWebApi.Database
+﻿namespace MTKDotNetCore.AdoDotNetWebApi.Database;
+
+public class ConnnectionStrings
 {
-    public class ConnnectionStrings
-    {
-        
-            public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-            {
-                DataSource = "localhost", // or "(local)" or your server name
-                InitialCatalog = "OJTBatch1",
-                IntegratedSecurity = true,
-                TrustServerCertificate = true,
-            };
-        
-    }
+    
+        public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "localhost", // or "(local)" or your server name
+            InitialCatalog = "OJTBatch1",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true,
+        };
+    
 }
