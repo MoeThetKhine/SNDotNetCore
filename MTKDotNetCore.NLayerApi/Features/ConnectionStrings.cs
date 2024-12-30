15ffd7e193d399bc118702ef754cf66178e201f6
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:02:21 +0630

    -

---
+++ MTKDotNetCore.NLayerApi/Features/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace MTKDotNetCore.NLayerApi.Features;
+
+public class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder _sqlConnectionStringBuilder =
+        new()
+        {
+            DataSource = "localhost",
+            InitialCatalog = "OJTBatch1",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true
+        };
+}
