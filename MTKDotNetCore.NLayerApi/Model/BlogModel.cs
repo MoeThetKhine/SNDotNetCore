ae61e189ab855633035daedb3bd545f20c831daf
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 03:10:49 +0630

    Format Code

--- MTKDotNetCore.NLayerApi/Model/BlogModel.cs
+++ MTKDotNetCore.NLayerApi/Model/BlogModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCore.NLayerApi.Model
+﻿namespace MTKDotNetCore.NLayerApi.Model;
+
+[Table("Tbl_Blog")]
+public class BlogModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
 }
