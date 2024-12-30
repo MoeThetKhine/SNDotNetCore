269a6fe2ab3e9360a7c860deae0ce880113b43de
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:44:04 +0630

    Feat : Add Global Using

--- MTKDotNetCore.DapperCustomService/Program.cs
+++ MTKDotNetCore.DapperCustomService/Program.cs
@@ -1,6 +1,5 @@
-using MTKDotNetCore.DapperCustomService.Shared;
-
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
